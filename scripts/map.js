674b5fda4e9eb4d6bfc8c9cf0fdb6ae43fb08def
shubin.andrej <haxapaktepe@gmail.com>
2024-05-08 21:33:15 +0400

    fix

--- scripts/map.js
+++ scripts/map.js
@@ -138,59 +138,6 @@ if (document.querySelector('.map')) {
 					},
 					{
 						iconLayout: 'default#image',
-						// iconImageHref: '/drivemoto/assets/images/icons/mappin.svg',
-						iconImageHref: '../../assets/images/icons/mappin.svg',
-						iconImageSize: [56, 73],
-					}
-				)
-				myCollection.add(myPlacemark)
-			}
-
-			myMap.geoObjects.add(myCollection)
-
-			// Обработчик клика на адрес
-			var addressTitles = document.querySelectorAll('.stores__item-title')
-			addressTitles.forEach(function (title) {
-				title.addEventListener('click', function () {
-					var coords = this.getAttribute('data-coords').split(',')
-					myMap.setCenter([coords[0], coords[1]], 16)
-				})
-			})
-		}
-	}
-
-	if (document.querySelector('#map')) {
-		var points = [
-			[
-				'<div class="map-baloon"><p>г. Набережные Челны, Мензелинский тракт, 22/1 (1 этаж)</p></div>',
-				55.70671442813047,
-				52.391260497601,
-			],
-			[
-				'<div class="map-baloon"><p>г. Казань, пр-кт Победы,200</p></div>',
-				55.801611973104926,
-				49.21166816877036,
-			],
-		]
-
-		ymaps.ready(init)
-
-		function init() {
-			var myMap = new ymaps.Map('map', {
-				center: [55.70671837681868, 52.39125842994889],
-				zoom: 16,
-			})
-
-			var myCollection = new ymaps.GeoObjectCollection()
-
-			for (var i = 0; i < points.length; i++) {
-				var myPlacemark = new ymaps.Placemark(
-					[points[i][1], points[i][2]],
-					{
-						balloonContent: points[i][0],
-					},
-					{
-						iconLayout: 'default#image',
 						iconImageHref: '/drivemoto/assets/images/icons/mappin.svg',
 						iconImageSize: [56, 73],
 					}
@@ -263,7 +210,7 @@ if (document.querySelector('.map')) {
 
 		function init() {
 			var myMap = new ymaps.Map('map3', {
-				center: [55.70671837681868, 52.39125842994889],
+				center: [55.75533190851993, 37.61782919835717],
 				zoom: 16,
 			})
 
